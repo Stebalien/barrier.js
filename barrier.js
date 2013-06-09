6884be2de2c6b8bbf48d0617955481b66bdf746e
Steven Allen <steven@stebalien.com>
2013-06-09 17:42:05 -0400

    Fixup license/readme.

--- barrier.js
+++ barrier.js
@@ -1,55 +1,11 @@
-/** 
- * Barrier.js
+/* Barrier.js
  * ==========
  *
  * Provides a simple barrier for synchronization.
  *
- * To use, aquire the barrier before initiating a asynchronous operation and
- * release the barrier after the asynchronous operation has completed.
- *
- * When all aquired barriers have been released, waiting functions will be
- * called in order until either no waiting functions remain or a waiting
- * function takes the barrier.
- *
- * Example:
- *
- *   var barr = new Barrier();
- *   var myCb = function() {
- *     barr.release();
- *   };
- *   barr.aquire(2);
- *   myAsynchrounousMethod(myCb)
- *   myAsynchrounousMethod2(myCb)
- *   barr.wait(function() {
- *     //continue
- *   });
- *
- * Licence
- * =======
- *
- * The MIT License (MIT)
- * 
- * Copyright (c) 2013 Steven Allen
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- **/
+ * Source: https://github.com/Stebalien/barrier.js
+ * Licence: MIT, Copyright (c) 2013 Steven Allen
+ */
 
 /**
  * Bind compatability snippet
